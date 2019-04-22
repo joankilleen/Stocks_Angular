66d99564ead8b21d27526fc5a40210afb8a2df32
Joan Killeen <joan.killeen@bluewin.ch>
2019-04-22 10:34:47 +0200

    Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { StocksService } from './services/stocks.service';
 
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
